890b5bdf0bd6f88f1aeb2f28840cebd8d868dd0d
aarongoodermuth <aaron.goodermuth@gmail.com>
2013-03-12 19:30:43 -0400

    --still working on things guys. edit account pages and such

--- supplier/dashboard.php
+++ supplier/dashboard.php
@@ -18,15 +18,12 @@ include_once '/home/goodermuth/dev/websites/himalaya/common/functions.php';
 /** FUNCTIONS **/
 /***************/
 
-// prints links to all RU functions
+// prints links to all Supplier functions
 // (void)
 function show_links()
 {
   echo '<p><a href="selling.php">Items I am selling</a></p>';              // items I'm selling
-  echo '<p><a href="bought.php">Items I am buying</a></p>';                // items I have bought
-  echo '<p><a href="highbidder.php">Items where I am high bidder</a></p>'; // items I am high bidder
-  echo '<p><a href="../items/browse">Search or Browse</a></p>';            // search/browse
-  echo '<p><a href="sellitem.php">Sell Item</a></p>';                      // sell an item
+  echo '<p><a href="orders.php">My Unfullfilled Order</a></p>';            // items that have sold but need shipping
   echo '<p><a href="editaccount.php">Edit Account</a></p>';                // edit account
   echo '<p><a href="../welcome/logout.php">Log Out</a></p>';               // log out
 } 
@@ -47,7 +44,7 @@ $user = check_logged_in_user($c);
 if($user != null)
 {
   $type = mysql_get_type_from_username($c, $user);
-  if($type == $USER_TYPE_MAPPING[0])
+  if($type == $USER_TYPE_MAPPING[1])
   {
     // is a RU
     print_html_header();
@@ -55,10 +52,10 @@ if($user != null)
     show_links();
     print_html_footer();
   }
-  elseif($type == $USER_TYPE_MAPPING[1])
+  elseif($type == $USER_TYPE_MAPPING[0])
   {
     // is a supplier
-    header('refresh:0; url=../supplier/dashboard.php');
+    header('refresh:0; url=../user/dashboard.php');
   }
   else
   {
--- users/editaccount.php
+++ users/editaccount.php
@@ -5,8 +5,8 @@
 /**************/
 
 include_once '/home/goodermuth/dev/websites/himalaya/common/constants.php';
-include_once '/home/goodermuth/dev/websites/himalaya/common/mysql.php';
 include_once '/home/goodermuth/dev/websites/himalaya/common/functions.php';
+include_once '/home/goodermuth/dev/websites/himalaya/common/mysql.php';
 
 /******************/
 /** END INCLUDES **/
@@ -18,18 +18,6 @@ include_once '/home/goodermuth/dev/websites/himalaya/common/functions.php';
 /** FUNCTIONS **/
 /***************/
 
-// prints links to all RU functions
-// (void)
-function show_links()
-{
-  echo '<p><a href="selling.php">Items I am selling</a></p>';              // items I'm selling
-  echo '<p><a href="bought.php">Items I am buying</a></p>';                // items I have bought
-  echo '<p><a href="highbidder.php">Items where I am high bidder</a></p>'; // items I am high bidder
-  echo '<p><a href="../items/browse">Search or Browse</a></p>';            // search/browse
-  echo '<p><a href="sellitem.php">Sell Item</a></p>';                      // sell an item
-  echo '<p><a href="editaccount.php">Edit Account</a></p>';                // edit account
-  echo '<p><a href="../welcome/logout.php">Log Out</a></p>';               // log out
-} 
 
 /*******************/
 /** END FUNCTIONS **/
@@ -49,20 +37,36 @@ if($user != null)
   $type = mysql_get_type_from_username($c, $user);
   if($type == $USER_TYPE_MAPPING[0])
   {
-    // is a RU
     print_html_header();
-    echo '<h3>Welcome to the Dashboard, ' . $user . '!</h3>';
-    show_links();
+    if(1) // post values found
+    {
+      // get post values
+      // ...
+
+      // update DB
+      // ...
+
+      // print status message
+      if(1) // success
+      {
+        echo '<p style="color:red">Account successfully updated</p>';
+      }
+      else // failure
+      {
+        echo '<p style="color:red">The database barfed at your values. It might
+                 be our fault, but it also might be yours.</p>';
+      }
+    }
+    show_form('editregisterduser');
     print_html_footer();
   }
   elseif($type == $USER_TYPE_MAPPING[1])
   {
-    // is a supplier
-    header('refresh:0; url=../supplier/dashboard.php');
+    header('refresh:0; url=../supplier/editaccount.php');
   }
   else
   {
-    die('The database done goofed!');
+    header('refresh:0; url=../welcome/login.php');
   }
 }
 else
