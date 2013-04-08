ef12067f604e5cd570015488e691c9da65bfc956
Aaron Goodermuth <aaron.goodermuth@gmail.com>
2013-04-07 21:11:44 -0400

    (Brian G.) add user ratings display to an item page

--- items/view.php
+++ items/view.php
@@ -23,126 +23,142 @@ include_once '/home/goodermuth/dev/websites/himalaya/common/mysql_members.php';
 // (boolean)
 function value_set()
 {
-  return !empty($_GET['id']);
+	return !empty($_GET['id']);
 }
 
 // prints HTML text in red
 //  $s - string to print
 function print_message($s)
 {
-  echo '<p style="color:red">' . $s . '</p>';
+	echo '<p style="color:red">' . $s . '</p>';
 }
 
 // gets information about the item from id
 //  (string[])
 function mysql_get_item_info($c, $id)
 {
-  global $ZIP_TABLE, $PRODUCTS_TABLE, $SALE_ITEMS_TABLE, 
-         $CATEGORY_TABLE, $ADDRESS_TABLE;
+	global $ZIP_TABLE, $PRODUCTS_TABLE, $SALE_ITEMS_TABLE, 
+	       $CATEGORY_TABLE, $ADDRESS_TABLE;
 
-  $id = sanitize($id);  
- 
-  $str = "SELECT si.item_desc, si.product_id, si.username, 
-                 si.scondition, si.url, si.posted_date, 
+	$id = sanitize($id);  
+
+	$str = "SELECT si.item_desc, si.product_id, si.username, 
+		 si.scondition, si.url, si.posted_date, 
 		 p.p_name, p.p_desc, p.category_id, 
 		 c.category_name,
 		 addr.zip,
 		 z.city, z.zstate
-          FROM   $SALE_ITEMS_TABLE si, $PRODUCTS_TABLE p, $CATEGORY_TABLE c, 
-	         $ADDRESS_TABLE addr, $ZIP_TABLE z
-          WHERE  si.item_id = ? AND si.product_id = p.product_id 
-	         AND p.category_id = c.category_id 
+	  FROM   $SALE_ITEMS_TABLE si, $PRODUCTS_TABLE p, $CATEGORY_TABLE c, 
+		 $ADDRESS_TABLE addr, $ZIP_TABLE z
+	  WHERE  si.item_id = ? AND si.product_id = p.product_id 
+		 AND p.category_id = c.category_id 
 		 AND si.username = addr.username AND addr.zip = z.zip";
 
-  /*echo "categories table = $CATEGORY_TABLE, ";
-  echo "sale items table = $SALE_ITEMS_TABLE";
-  
-  return NULL;*/
+	if ($stmt = mysqli_prepare($c, $str)) {
+		mysqli_stmt_bind_param($stmt, 'i', $id);
+		mysqli_stmt_execute($stmt);
+		mysqli_stmt_bind_result($stmt, $item_desc, $product_id, $seller,
+		                        $condition, $url, $posted_date, $p_name,
+					$p_desc, $category_id, $category_name, 
+					$zip, $city, $zstate);
+		mysqli_stmt_fetch($stmt);
+		mysqli_stmt_close($stmt);
+	} else {
+		printf("Errormessage: %s\n", mysqli_error($c));
+		return NULL;
+	}
 
-  if ($stmt = mysqli_prepare($c, $str)) {
-	mysqli_stmt_bind_param($stmt, 'i', $id);
-	mysqli_stmt_execute($stmt);
-	mysqli_stmt_bind_result($stmt, $item_desc, $product_id, $seller, 
-          $condition, $url, $posted_date, $p_name, $p_desc, $category_id, 
-	  $category_name, $zip, $city, $zstate);
-	mysqli_stmt_fetch($stmt);
-	mysqli_stmt_close($stmt);
-  } else {
-	//printf("Error: %s.\n", mysqli_stmt_error($stmt));
-	printf("Errormessage: %s\n", mysqli_error($c));
-	return NULL;
-  }
-  
-  return array($item_desc, $product_id, $seller, $condition, $url, 
-               $posted_date, $p_name, $p_desc, $category_id, 
-	       $category_name, $zip, $city, $zstate);
-/*
-  $i = 0;
-  while($temp = mysqli_fetch_row($db_answer))
-  {
-    $retval[$i] = $temp;
-    $i++;
-  }
-
-  return $retval;*/
+	return array($item_desc, $product_id, $seller, $condition, $url, 
+	             $posted_date, $p_name, $p_desc, $category_id, 
+	             $category_name, $zip, $city, $zstate);
 }
 
 // gets information about price from auctions or sales
 //  (string[])
 function mysql_get_price_info($c, $id)
 {
-  global $AUCTIONS_TABLE, $SALES_TABLE;
+	global $AUCTIONS_TABLE, $SALES_TABLE;
 
-  $id = sanitize($id);
-  
-  $str = "SELECT COUNT(*) FROM $AUCTIONS_TABLE a, $SALES_TABLE sa WHERE 
-    a.item_id = ? OR sa.item_id = ?";
-  if ($stmt = mysqli_prepare($c, $str)) {
-	mysqli_stmt_bind_param($stmt, 'ii', $id, $id);
-	mysqli_stmt_execute($stmt);
-	mysqli_stmt_bind_result($stmt, $count);
-	mysqli_stmt_fetch($stmt);
-	mysqli_stmt_close($stmt);
-  } else {
-	return NULL;
-  }
-  
-  if ($count == 0) {
-    return NULL;
-  }
+	$id = sanitize($id);
 
-  $str = "SELECT a.recent_bid, a.end_date, a.recent_bidder 
-          FROM $AUCTIONS_TABLE a WHERE a.item_id = ?";
-  if ($stmt = mysqli_prepare($c, $str)) {
-	mysqli_stmt_bind_param($stmt, 'i', $id);
-	mysqli_stmt_execute($stmt);
-	mysqli_stmt_bind_result($stmt, $recent_bid, $end_date, $recent_bidder);
-	mysqli_stmt_fetch($stmt);
-	mysqli_stmt_close($stmt);
-  } else {
-	return NULL;
-  }
-  
-  if (!empty($recent_bid) && !empty($end_date)) {
-    return $retval = array('a', $recent_bid, $end_date, $recent_bidder);
-  }
+	$str = "SELECT COUNT(*) FROM $AUCTIONS_TABLE a, $SALES_TABLE sa WHERE
+	a.item_id = ? OR sa.item_id = ?";
+	if ($stmt = mysqli_prepare($c, $str)) {
+		mysqli_stmt_bind_param($stmt, 'ii', $id, $id);
+		mysqli_stmt_execute($stmt);
+		mysqli_stmt_bind_result($stmt, $count);
+		mysqli_stmt_fetch($stmt);
+		mysqli_stmt_close($stmt);
+	} else {
+		return NULL;
+	}
 
-  $str = "SELECT sa.price FROM $SALES_TABLE sa WHERE sa.item_id = ?";
-  if ($stmt = mysqli_prepare($c, $str)) {
-	mysqli_stmt_bind_param($stmt, 'i', $id);
-	mysqli_stmt_execute($stmt);
-	mysqli_stmt_bind_result($stmt, $price);
-	mysqli_stmt_fetch($stmt);
-	mysqli_stmt_close($stmt);
-  } else {
-	return NULL;
-  }
+	if ($count == 0) {
+		return NULL;
+	}
 
-  if (!empty($price)) {
-	return $retval = array('s', $price);
-  } else {
-	return NULL;
-  }
+	$str = "SELECT a.recent_bid, a.end_date, a.recent_bidder 
+	  FROM $AUCTIONS_TABLE a WHERE a.item_id = ?";
+	if ($stmt = mysqli_prepare($c, $str)) {
+		mysqli_stmt_bind_param($stmt, 'i', $id);
+		mysqli_stmt_execute($stmt);
+		mysqli_stmt_bind_result($stmt, $recent_bid, 
+		                        $end_date, $recent_bidder);
+		mysqli_stmt_fetch($stmt);
+		mysqli_stmt_close($stmt);
+	} else {
+		return NULL;
+	}
+
+	if (!empty($recent_bid) && !empty($end_date)) {
+		return $retval = array('a', $recent_bid, 
+		                       $end_date, $recent_bidder);
+	}
+
+	$str = "SELECT sa.price FROM $SALES_TABLE sa WHERE sa.item_id = ?";
+	if ($stmt = mysqli_prepare($c, $str)) {
+		mysqli_stmt_bind_param($stmt, 'i', $id);
+		mysqli_stmt_execute($stmt);
+		mysqli_stmt_bind_result($stmt, $price);
+		mysqli_stmt_fetch($stmt);
+		mysqli_stmt_close($stmt);
+	} else {
+		return NULL;
+	}
+
+	if (!empty($price)) {
+		return $retval = array('s', $price);
+	} else {
+		return NULL;
+	}
+}
+
+// gets average rating for the seller of this item
+//  (array[float, int] on success, NULL failure)
+function mysql_get_avg_rating($c, $username)
+{
+	global $REG_USER_TABLE, $RATINGS_TABLE; 
+
+	$str = "SELECT   AVG(R.rating), COUNT(*)
+	        FROM     $RATINGS_TABLE R
+		WHERE    R.rated_user = ?
+		GROUP BY R.rated_user";
+
+	if ($stmt = mysqli_prepare($c, $str)) {
+		mysqli_stmt_bind_param($stmt, 's', $username);
+		mysqli_stmt_execute($stmt);
+		mysqli_stmt_bind_result($stmt, $avgrating, $count);
+		mysqli_stmt_fetch($stmt);
+		mysqli_stmt_close($stmt);
+	} else {
+		printf("Errormessage: %s\n", mysqli_error($c));
+		return NULL;
+	}
+	
+	if (isset($avgrating) && isset($count))
+		return array($avgrating, $count);
+	else 
+		return NULL;
 }
 
 /*******************/
@@ -191,11 +207,15 @@ if($user != NULL) {
 		  <tr>
 		    <td height=\"30\">Seller:</td>
 		    <td height=\"30\">
-		      <a href=\"/users/view?username=$item_info[2]\">$item_info[2]</a>";  /* format of URL?? */
+		      <a href=\"/users/view?username=$item_info[2]\">$item_info[2]</a>";
 		
-		/*if (avgrating) {
-			echo "(avgrating)";
-		}*/
+		if (($rating_info = mysql_get_avg_rating($c, $item_info[2])) != NULL) {
+			printf("&nbsp;&nbsp;(%.1f/10 rating)", $rating_info[0]);
+			echo "</td></tr>
+			<tr>
+			  <td></td>
+			  <td>based on $rating_info[1] customer reviews";
+		}
 		
 		echo '</td></tr>';
 		
